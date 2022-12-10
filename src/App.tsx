084a1d9be49f815fb3a657e3c375ff5614bbc7af
rmatsuda21 <reo.matsuda@gmail.com>
2022-12-09 21:34:23 -0500

    generate QR code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import qrcode from "qrcode-generator";
+import { toJpeg, toPng, toSvg } from "html-to-image";
+import { copyImageToClipboard } from "copy-image-clipboard";
+import { Button, Input, TextField, useTheme } from "@mui/material";
+import { useSnackbar } from "notistack";
+import styled from "@emotion/styled";
 
 function App() {
+  const [qrCode, setQrCode] = useState<boolean[][]>();
+  const [text, setText] = useState("");
+  const ref = useRef<HTMLDivElement>(null);
+
+  const theme = useTheme();
+  const { enqueueSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    const code = qrcode(0, "Q");
+    code.addData(text);
+    code.make();
+    const count = code.getModuleCount();
+    const grid = new Array(count)
+      .fill(false)
+      .map((_, indx) =>
+        new Array(count).fill(false).map((_, indx2) => code.isDark(indx, indx2))
+      );
+
+    setQrCode(grid);
+  }, [text]);
+
+  const qrCodeString =
+    qrCode &&
+    qrCode.map((segment) =>
+      segment.map((char) => (char ? "⬛" : "⬜️")).join("")
+    );
+
+  console.log(qrCode);
+
+  const handleOnClick = () => {
+    toJpeg(ref?.current as HTMLElement).then((dataUrl) => {
+      copyImageToClipboard(dataUrl);
+      enqueueSnackbar("Copied to clipboard", {
+        variant: "success",
+        autoHideDuration: 1500,
+      });
+    });
+  };
+
+  const QRCodeWrapper = styled.div`
+    background-color: ${theme.palette.background.default};
+    display: grid;
+    grid-template-columns: repeat(${qrCode?.length || 0}, 1fr);
+    grid-template-rows: repeat(${qrCode?.length || 0}, 1fr);
+    width: 500px;
+    height: 500px;
+    padding: 20px;
+    border-radius: 20px;
+    outline: 5px solid rgba(255, 255, 255, 0.08);
+
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+      cursor: pointer;
+      background-color: rgba(255, 255, 255, 0.07);
+      outline: 5px solid rgba(255, 255, 255, 0.2);
+      transform: scale(1.007);
+    }
+  `;
+  const convertedQrCode = (
+    <QRCodeWrapper ref={ref} onClick={handleOnClick}>
+      {qrCode?.map((segment, i) =>
+        segment.map(
+          (char, j) =>
+            char && (
+              <div
+                key={`${i}-${j}`}
+                style={{
+                  backgroundColor: "rgba(255,255,255,.4)",
+                  gridArea: `${i + 1} / ${j + 1} / ${i + 2} / ${j + 2}`,
+                  borderRadius: 10,
+                }}
+              />
+            )
+        )
+      )}
+    </QRCodeWrapper>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: theme.palette.background.default,
+      }}
+    >
+      {convertedQrCode}
+      <TextField
+        style={{ width: "25%" }}
+        label="Text"
+        variant="filled"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      {/* <Button variant="contained" onClick={handleOnClick}>
+        Copy to Clipboard
+      </Button> */}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { SnackbarProvider } from "notistack";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+import "@fontsource/fira-mono";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+  typography: {
+    fontFamily: ["Fira Mono"].join(","),
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <SnackbarProvider maxSnack={3}>
+        <App />
+      </SnackbarProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
